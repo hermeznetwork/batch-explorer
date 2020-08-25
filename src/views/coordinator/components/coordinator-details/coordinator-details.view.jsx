7cfbede2a045c8edb8dfe6f97774ca09fa18194f
Miros <miros@iden3.com>
2020-08-25 17:48:07 +0200

    coordinator URL to be external link

--- src/views/coordinator/components/coordinator-details/coordinator-details.view.jsx
+++ src/views/coordinator/components/coordinator-details/coordinator-details.view.jsx
@@ -4,13 +4,19 @@ function CoordinatorDetails ({ coordinator }) {
   return (
     <section>
       <div>
+        <a
+          href={coordinator[0].URL}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {coordinator[0].URL}
+        </a>
+      </div>
+      <div>
         Forger address: {coordinator[0].Forger}
       </div>
       <div>
         Withdrawal address: {coordinator[0].Beneficiary}
-      </div>
-      <div>
-        URL: {coordinator[0].URL}
       </div>
     </section>
   )
