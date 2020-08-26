4507f94b1e195cfb8efce0541fae018e8bc7f2b4
Miros <miros@iden3.com>
2020-08-26 15:19:40 +0200

    batch transaction list moved to shared; initial structure for user account

---
+++ src/views/user-account/user-account.view.jsx
@@ -0,0 +1,7 @@
+// dva dela
+// Account
+// transactions ce da se reuse iz batachtransactions (tako da se batchtransactions prebacuje u shared)
+
+// API
+// transactions I can get them from here https://idocs.hermez.io/#/spec/architecture/api-server?id=get-accountsethaddrtxs
+// and for accounts from here https://idocs.hermez.io/#/spec/architecture/api-server?id=get-accountsethaddr
