cb444707040f86297c2e19a9ea96ccae48795613
Miros <miros@iden3.com>
2020-09-20 22:40:20 +0200

    transaction initial store

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,15 @@ import homeReducer from './home/home.reducer'
 import batchReducer from './batch/batch.reducer'
 import coordinatorReducer from './coordinator/coordinator.reducer'
 import userAccountReducer from './user-account/user-account.reducer'
+import transactionReducer from './transaction/transaction.reducer'
 
 const rootReducer = combineReducers({
   global: globalReducer,
   home: homeReducer,
   batch: batchReducer,
   coordinator: coordinatorReducer,
-  userAccount: userAccountReducer
+  userAccount: userAccountReducer,
+  transaction: transactionReducer
 })
 
 const middlewares = [thunk]
