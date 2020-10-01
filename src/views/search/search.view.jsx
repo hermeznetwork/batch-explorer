52556c8961e46412c0981b19a2ea29bd47cae939
Miros <miros@iden3.com>
2020-10-01 21:34:03 +0200

    add transaction regex + route

--- src/views/search/search.view.jsx
+++ src/views/search/search.view.jsx
@@ -14,11 +14,21 @@ function Search ({
   let route = '/'
   const ethereumAddressPattern = new RegExp('^0x[a-fA-F0-9]{40}$')
   const batchNumPattern = new RegExp('^([0-4]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9])$')
+  const transactionIdPattern = new RegExp('^0x00[a-fA-F0-9]{22}|^0x01[a-fA-F0-9]{22}|^0x02[a-fA-F0-9]{22}$')
+
+  // Search options:
+  // ens -> user account
+  // OPTIONAL: ethereumBlockNum -> batch page
+  // DONE ethaddress -> user account
+  // DONE batch number -> batch page
+  // DONE transactionId -> transaction page
 
   if (ethereumAddressPattern.test(value)) {
     route = '/user-account/' + value
   } else if (batchNumPattern.test(value)) {
     route = '/batch/' + value
+  } else if (transactionIdPattern.test(value)) {
+    route = '/transaction/' + value
   } else {
     route = '/search-error'
   }
