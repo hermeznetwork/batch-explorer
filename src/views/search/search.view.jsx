a57de403d831cd08b39fee6ba663fce7f61ae4e8
Miros <miros@iden3.com>
2020-10-02 11:58:31 +0200

    adding bjj pattern and correcting hez: and simple ethaddress

--- src/views/search/search.view.jsx
+++ src/views/search/search.view.jsx
@@ -13,15 +13,19 @@ function Search ({
   const input = <input value={value} onChange={e => setValue(e.target.value)} type='text' />
   let route = '/'
   const ethereumAddressPattern = new RegExp('^0x[a-fA-F0-9]{40}$')
+  const hezEthereumAddressPattern = new RegExp('^hez:0x[a-fA-F0-9]{40}$')
+  const bjjAddressPattern = new RegExp('^hez:[A-Za-z0-9_-]{44}$')
   const batchNumPattern = new RegExp('^([0-4]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9]?[0-9])$')
   const transactionIdPattern = new RegExp('^0x00[a-fA-F0-9]{22}|^0x01[a-fA-F0-9]{22}|^0x02[a-fA-F0-9]{22}$')
 
-  if (ethereumAddressPattern.test(value)) {
+  if (hezEthereumAddressPattern.test(value) || bjjAddressPattern.test(value)) {
     route = '/user-account/' + value
   } else if (batchNumPattern.test(value)) {
     route = '/batch/' + value
   } else if (transactionIdPattern.test(value)) {
     route = '/transaction/' + value
+  } else if (ethereumAddressPattern.test(value)) {
+    route = '/coordinator/' + value
   } else {
     route = '/search-error'
   }
