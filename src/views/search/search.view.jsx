d33f63caee583d03f7e6a9707fcc4b1e87ca4d74
Miros <miros@iden3.com>
2020-10-02 21:46:44 +0200

    refactor as per PR

--- src/views/search/search.view.jsx
+++ src/views/search/search.view.jsx
@@ -9,30 +9,31 @@ function Search ({
   changeRoute
 }) {
   const classes = useSearchStyles()
-  const [value, setValue] = useState('')
-  let route = '/'
+  const [searchTerm, setSearchTerm] = useState('')
   const ethereumAddressPattern = new RegExp('^0x[a-fA-F0-9]{40}$')
   const hezEthereumAddressPattern = new RegExp('^hez:0x[a-fA-F0-9]{40}$')
   const bjjAddressPattern = new RegExp('^hez:[A-Za-z0-9_-]{44}$')
   const batchNumPattern = new RegExp('^[0-4]?\\d{0,9}$')
   const transactionIdPattern = new RegExp('^0x00[a-fA-F0-9]{22}|^0x01[a-fA-F0-9]{22}|^0x02[a-fA-F0-9]{22}$')
 
-  if (hezEthereumAddressPattern.test(value) || bjjAddressPattern.test(value)) {
-    route = '/user-account/' + value
-  } else if (batchNumPattern.test(value)) {
-    route = '/batch/' + value
-  } else if (transactionIdPattern.test(value)) {
-    route = '/transaction/' + value
-  } else if (ethereumAddressPattern.test(value)) {
-    route = '/coordinator/' + value
-  } else {
-    route = '/search-error'
+  function handleSearch () {
+    if (hezEthereumAddressPattern.test(searchTerm) || bjjAddressPattern.test(searchTerm)) {
+      changeRoute(`/user-account/${searchTerm}`)
+    } else if (batchNumPattern.test(searchTerm)) {
+      changeRoute(`/batch/${searchTerm}`)
+    } else if (transactionIdPattern.test(searchTerm)) {
+      changeRoute(`/transaction/${searchTerm}`)
+    } else if (ethereumAddressPattern.test(searchTerm)) {
+      changeRoute(`/coordinator/${searchTerm}`)
+    } else {
+      changeRoute('/search-error')
+    }
   }
 
   return (
     <div className={classes.root}>
-      <input value={value} onChange={e => setValue(e.target.value)} type='text' />
-      <button onClick={() => { changeRoute(route) }}>CLICK ME</button>
+      <input value={searchTerm} onChange={e => setSearchTerm(e.target.value)} type='text' />
+      <button onClick={handleSearch}>CLICK ME</button>
     </div>
   )
 }
