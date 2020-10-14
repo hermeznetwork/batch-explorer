b1e07210248979cc0913f8917311d0c59ef06a4f
Miros <miros@iden3.com>
2020-10-14 21:46:40 +0200

    initial styling accounts

--- src/views/user-account/user-account.styles.js
+++ src/views/user-account/user-account.styles.js
@@ -1,9 +1,15 @@
 import { createUseStyles } from 'react-jss'
 
-const useUserAccountStyles = createUseStyles({
+const useUserAccountStyles = createUseStyles(theme => ({
+  wrapper: {
+    width: '100%'
+  },
   title: {
-    marginBottom: 16
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(5),
+    fontWeight: theme.fontWeights.extraBold,
+    fontSize: theme.spacing(3)
   }
-})
+}))
 
 export default useUserAccountStyles
