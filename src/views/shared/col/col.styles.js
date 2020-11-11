7c70c51771670fcaefee2b91dfabfc372cfb8470
Miros <miros@iden3.com>
2020-11-11 11:17:46 +0100

    removing comments

--- src/views/shared/col/col.styles.js
+++ src/views/shared/col/col.styles.js
@@ -6,8 +6,6 @@ const useColStyles = createUseStyles(theme => ({
     '&:last-child': {
       fontWeight: theme.fontWeights.bold,
       maxWidth: theme.spacing(52),
-      // token-accounts
-      // width: theme.spacing(20),
       overflowWrap: 'break-word',
       lineHeight: `${theme.spacing(3)}px`,
       marginTop: theme.spacing(1.5),
@@ -18,7 +16,6 @@ const useColStyles = createUseStyles(theme => ({
     }
   },
   colWrapped: {
-    // button 4.5 + root wrapper margins 2 x 4.5 = 11.5
     maxWidth: `calc(100% - ${theme.spacing(11.5)}px)`,
     [theme.breakpoints.sm]: {
       maxWidth: theme.spacing(45)
