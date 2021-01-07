17fe1d04c5f18ddcfa730d09af8cbfc629d69a5e
Elias <eliasgarciadev@gmail.com>
2021-01-07 15:35:43 +0100

    Fix typo in the home page

--- src/store/home/home.reducer.js
+++ src/store/home/home.reducer.js
@@ -25,7 +25,7 @@ function homeReducer (state = initialHomeState, action) {
       const batches = state.batchesTask.status === 'reloading'
         ? [...state.batchesTask.data.batches, ...action.data.batches]
         : action.data.batches
-      const pagination = getPaginationData(action.data.pendingItems, batches, PaginationOrder.DES)
+      const pagination = getPaginationData(action.data.pendingItems, batches, PaginationOrder.DESC)
 
       return {
         ...state,
