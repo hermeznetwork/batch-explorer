f44056fcfc5b535ae84bea39633e93300402de1a
Alberto Elias <hi@albertoelias.me>
2021-02-23 19:08:07 +0100

    Show token amounts with 6 decimals of precision

--- src/utils/currencies.js
+++ src/utils/currencies.js
@@ -1,25 +1,17 @@
 import { feeFactors } from '@hermeznetwork/hermezjs/src/fee-factors'
 import { getTokenAmountString } from '@hermeznetwork/hermezjs/src/utils'
 
-import { MAX_DECIMALS_UNTIL_ZERO_AMOUNT } from '../constants'
+import { MAX_TOKEN_DECIMALS } from '../constants'
 
 function getFixedTokenAmount (amount, decimals) {
-  if (!amount || !decimals) {
+  if (amount === undefined || decimals === undefined) {
     return undefined
   }
 
   // We can lose precision as there will never be more than MAX_DECIMALS_UNTIL_ZERO_AMOUNT significant digits
   const balanceWithDecimals = Number(amount) / Math.pow(10, decimals)
-  const amountSignificantDigits = amount.length - 1
-  const significantDigits = decimals - amountSignificantDigits
 
-  if (significantDigits > MAX_DECIMALS_UNTIL_ZERO_AMOUNT) {
-    return '0'
-  }
-
-  const significantDigitsFinal = significantDigits > 2 ? significantDigits : 2
-
-  return balanceWithDecimals.toFixed(significantDigitsFinal)
+  return (Number(balanceWithDecimals.toFixed(MAX_TOKEN_DECIMALS))).toString()
 }
 
 function getFeeInUsd (feeIndex, amount, token) {
