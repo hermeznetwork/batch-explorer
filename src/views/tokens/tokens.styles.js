5b69359acd0ff379a4a9d8caa69d513109f025b4
Miros <miros@iden3.com>
2021-03-09 21:57:02 +0100

    add token button

--- src/views/tokens/tokens.styles.js
+++ src/views/tokens/tokens.styles.js
@@ -5,7 +5,12 @@ const useTokensStyles = createUseStyles(theme => ({
     width: '100%'
   },
   addTokenButton: {
-
+    color: theme.palette.secondary.main,
+    background: theme.palette.grey.veryLight,
+    padding: `${theme.spacing(1.5)}px ${theme.spacing(2.5)}px`,
+    borderRadius: 16,
+    marginTop: theme.spacing(5),
+    display: 'inline-flex'
   }
 }))
 
