7f5cf08c969333b4b6f0bd7e28337ae9b4063faf
Miros <miros@iden3.com>
2021-03-12 13:14:27 +0100

    removin unused

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,3 @@ export const MAX_TOKEN_DECIMALS = 6
 export const HEZ_TOKEN_DECIMALS = 18
 
 export const TESTNET_API_HOSTNAME = 'api.testnet.hermez.io'
-
-// Use following numbers to set network operational status
-// 1 - Operational
-// 2 - Degraded Performance
-// 3 - Network Unavailable
-
-export const NETWORK_STATUS_INDICATOR = 1
