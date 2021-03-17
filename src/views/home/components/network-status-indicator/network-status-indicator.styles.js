82df893185914bfdbe6991f198bd7bebe5253e33
Miros <miros@iden3.com>
2021-03-17 21:26:20 +0100

    various UI fixes

--- src/views/home/components/network-status-indicator/network-status-indicator.styles.js
+++ src/views/home/components/network-status-indicator/network-status-indicator.styles.js
@@ -4,11 +4,15 @@ const useNetworkStatusIndicatorStyles = createUseStyles(theme => ({
   status: {
     fontWeight: theme.fontWeights.medium,
     padding: `${theme.spacing(1.5)}px ${theme.spacing(5)}px`,
-    marginTop: theme.spacing(3.5),
-    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(2),
+    marginBottom: 0,
     borderRadius: 14,
     width: 'fit-content',
-    display: 'inline-block'
+    display: 'inline-block',
+    [theme.breakpoints.sm]: {
+      marginTop: theme.spacing(3.5),
+      marginBottom: theme.spacing(1)
+    }
   },
   unavailable: {
     background: theme.palette.red.veryLight,
