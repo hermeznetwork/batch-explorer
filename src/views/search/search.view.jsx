aec4e4713861e1ce8fb873ce36d501bc658606c9
Miros <miros@iden3.com>
2021-05-04 00:17:55 +0200

    Revert "sumbit search on clicking enter"
    This reverts commit fe673baf31f0abaaf47f962111c60f303949ad49.

--- src/views/search/search.view.jsx
+++ src/views/search/search.view.jsx
@@ -51,7 +51,6 @@ function Search ({
       <input
         value={searchTerm}
         onChange={e => setSearchTerm(e.target.value.trim())}
-        onKeyUp={e => e.keyCode === 13 && handleSearch()}
         type='text'
         placeholder='Search for a transaction, an address, or a batch'
         className={classes.input}
