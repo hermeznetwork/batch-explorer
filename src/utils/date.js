a5d571393da2a0de57077c96c25e974ede2c56fe
mmilenkovic <miros@iden3.com>
2021-05-04 15:51:43 +0200

    missing utils/date.js file (#216)
    * all-timestamps-should-be-presented-in-gmt
    
    * adding +GMT time zone offset from the locale
    
    * refactor as per PR
    
    * refactor as per PR
    
    * refactor to extract timezone calculation to utils
    
    * adding file

---
+++ src/utils/date.js
@@ -0,0 +1,36 @@
+/**
+ * Converts a time stamp to a corresponding 'xyz time ago' string
+ * @param {string} timestamp - String representing the time stamp returned by API 2019-08-24T14:15:22Z
+ * @returns {string}
+ */
+
+function getTimeAgo (timestamp) {
+  const diff = new Date().getTime() - new Date(timestamp).getTime()
+
+  const s = Math.floor((diff / 1000) % 60)
+  const m = Math.floor((diff / (1000 * 60)) % 60)
+  const h = Math.floor((diff / (1000 * 60 * 60)) % 24)
+  const d = Math.floor(diff / (1000 * 60 * 60 * 24))
+
+  const seconds = s + ' s ago'
+  const minutes = m > 0 ? m + ' m ' : ''
+  const hours = h > 0 ? h + ' h ' : ''
+  const days = d > 0 ? ' more than a day ago' : ''
+
+  const formattedTime = !days ? hours + minutes + seconds : days
+
+  return formattedTime
+}
+
+function getTimeZoneTimestamp (timestamp) {
+  const timeZoneOffsetInHours = -(new Date().getTimezoneOffset() / 60)
+  const timeZoneOffset = timeZoneOffsetInHours >= 0 ? '+' + timeZoneOffsetInHours : timeZoneOffsetInHours
+  const date = new Date(timestamp).toLocaleString() + ' (GMT ' + timeZoneOffset + ')'
+
+  return date
+}
+
+export {
+  getTimeAgo,
+  getTimeZoneTimestamp
+}
