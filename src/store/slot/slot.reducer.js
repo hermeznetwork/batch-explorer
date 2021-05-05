ee4a984daf3e0ea46ede0f1c8b0d4c99314056af
mmilenkovic <miros@iden3.com>
2021-05-05 12:05:34 +0200

    misspelling of occured (#218)

--- src/store/slot/slot.reducer.js
+++ src/store/slot/slot.reducer.js
@@ -37,7 +37,7 @@ function slotReducer (state = initialSlotState, action) {
         ...state,
         slotTask: {
           status: 'failed',
-          error: 'An error ocurred. Slot does not exist or cannot be loaded.'
+          error: 'An error occurred. Slot does not exist or cannot be loaded.'
         }
       }
     }
@@ -69,7 +69,7 @@ function slotReducer (state = initialSlotState, action) {
         ...state,
         bidsTask: {
           status: 'failed',
-          error: 'An error ocurred. Bid does not exist or cannot be loaded.'
+          error: 'An error occurred. Bid does not exist or cannot be loaded.'
         }
       }
     }
@@ -100,7 +100,7 @@ function slotReducer (state = initialSlotState, action) {
         ...state,
         batchesTask: {
           status: 'failed',
-          error: 'An error ocurred. Batches do not exist or cannot be loaded.'
+          error: 'An error occurred. Batches do not exist or cannot be loaded.'
         }
       }
     }
