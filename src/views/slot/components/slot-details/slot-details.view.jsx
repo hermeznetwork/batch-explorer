39d824b2414ef31f8514a580ebaa1188831dd70e
Miros <miros@iden3.com>
2021-06-29 16:00:36 +0200

    update slot-details view since there were changes in API response; update hermezjs to latest

--- src/views/slot/components/slot-details/slot-details.view.jsx
+++ src/views/slot/components/slot-details/slot-details.view.jsx
@@ -7,44 +7,7 @@ import { getFixedTokenAmount } from '../../../../utils/currencies'
 import { HEZ_TOKEN_DECIMALS } from '../../../../constants'
 
 function SlotDetails ({ slot, totalNumberOfBidsInSlot }) {
-  if (slot.closedAuction) {
-    return (
-      <div>
-        <Row>
-          <Col>
-            Status
-          </Col>
-          <Col>
-            Closed Auction
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            Bids
-          </Col>
-          <Col>
-            {totalNumberOfBidsInSlot}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            Winner bid
-          </Col>
-          <Col>
-            {getFixedTokenAmount(slot.winnerBid.bidValue, HEZ_TOKEN_DECIMALS)} HEZ
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            Coordinator
-          </Col>
-          <Col link>
-            <Link to={`/coordinator/${slot.winner.forgerAddr}`}>{slot.winner.forgerAddr}</Link>
-          </Col>
-        </Row>
-      </div>
-    )
-  } else {
+  if (slot.openAuction) {
     return (
       <div>
         <Row>
@@ -63,6 +26,49 @@ function SlotDetails ({ slot, totalNumberOfBidsInSlot }) {
             {totalNumberOfBidsInSlot}
           </Col>
         </Row>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Row>
+          <Col>
+            Status
+          </Col>
+          <Col>
+            Closed Auction
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            Bids
+          </Col>
+          <Col>
+            {totalNumberOfBidsInSlot}
+          </Col>
+        </Row>
+        {slot.bestBid
+          ? (
+            <>
+              <Row>
+                <Col>
+                  Winner bid
+                </Col>
+                <Col>
+                  {getFixedTokenAmount(slot.bestBid.bidValue, HEZ_TOKEN_DECIMALS)} HEZ
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  Coordinator
+                </Col>
+                <Col link>
+                  <Link to={`/coordinator/${slot.bestBid.forgerAddr}`}>{slot.bestBid.forgerAddr}</Link>
+                </Col>
+              </Row>
+            </>
+            )
+          : <></>}
       </div>
     )
   }
